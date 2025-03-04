44d2ac1b9b34a9b183bfc01e698a2784cf161186
Jean-Christian Denis <contact@jcdenis.fr>
2025-03-04 16:04:19 +0100

    release 1.8.4

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Templates engine',
     'Create and select more templates for your posts',
     'Osku and contributors',
-    '1.8.3',
+    '1.8.4',
     [
         'requires'    => [
             ['core', '2.33'],
